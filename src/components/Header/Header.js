fae4344f49b7bd87cd87b6889c70f5e90c485edc
musasazib <musasazib@gmail.com>
2021-09-26 19:42:12 +0600

    Added cart

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,6 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header">
-            <div>
-                <nav>
-                    <a href="/works">How We Works</a>
-                    <a href="/programmer">Our Programmers</a>
-                    <a href="/about">About Us</a>
-                </nav>
-            </div>
             <h2>The Best Programming Team In The World</h2>
             <h3>The mission of this team is to create the most expensive website in the world with the biggest programmers in the world.</h3>
             <h2>Budget : 100Million USD</h2>
--- src/components/TeamCart/TeamCart.js
+++ src/components/TeamCart/TeamCart.js
@@ -4,13 +4,22 @@ import './TeamCart.css';
 const TeamCart = (props) => {
     const { devCart } = props;
     let total = 0;
+
     for (const developer of devCart) {
         total = total + developer.salary;
     }
     return (
-        <div>
+        <div className="cart-container">
             <h3>Added Programer: {props.devCart.length}</h3>
             <h4>Total cost: ${total.toFixed(2)}</h4>
+            {
+                devCart.map(cart => <div className="cart-info">
+                    <div className="cart-dev">
+                        <h4>{cart.name}</h4>
+                        <img src={cart.picture} alt="" />
+                    </div>
+                </div>)
+            }
         </div>
     );
 };
